7a8bf259bcd811ad94004678f12a7db7b9f1158a
Parambrata Ghosh <gpampa138@gmail.com>
2024-07-30 14:41:34 +0530

    cors updated

--- src/config/corsOptions.js
+++ src/config/corsOptions.js
@@ -1,13 +1,12 @@
 import { allowedOrigins } from "./allowedOrigin.js";
 
 export const corsoptions = {
-    origin: (origin,callback) => {
-        if(allowedOrigins.indexOf(origin) !== -1 || !origin){
-            callback(null,true)
-        }else{
-            callback(new Error('Not Allowed By CORS'))
+    origin: (origin, callback) => {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
         }
     },
-    credentials:true,
-    optionsSuccessStatus:200
+    credentials: true,
 }
